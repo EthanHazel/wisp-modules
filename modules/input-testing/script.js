fe758893a1b2e48ea29ca6836017dd1ae0d44d62
Ethan Hazel <134425461+EthanHazel@users.noreply.github.com>
2025-02-09 12:32:54 -0500

    update(testModule): shows current width and height

---
+++ modules/input-testing/script.js
@@ -0,0 +1,4 @@
+window.addEventListener("resize", function () {
+  document.getElementById("width").innerHTML = window.innerWidth;
+  document.getElementById("height").innerHTML = window.innerHeight;
+});
