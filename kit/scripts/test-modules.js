17f952cca01f3721ae9ae3753d04bcae25919f7f
Ethan Hazel <134425461+EthanHazel@users.noreply.github.com>
2025-03-02 15:13:33 -0500

    add(test-modules): created boilerplate code

---
+++ kit/scripts/test-modules.js
@@ -0,0 +1,8 @@
+console.log("Hello World!");
+console.log(
+  "This script is a work in progress, this is just to see if the script is ever run."
+);
+
+console.log("Goodbye World!");
+
+process.exit(0);
