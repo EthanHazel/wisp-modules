cd440c933cb2bf8c3a52d2e36cea6d40360cf1eb
Ethan Hazel <134425461+EthanHazel@users.noreply.github.com>
2025-03-02 16:38:38 -0500

    update(workflow): added tests to poke workflow

--- kit/scripts/test-modules.js
+++
@@ -1,8 +0,0 @@
-console.log("Hello World!");
-console.log(
-  "This script is a work in progress, this is just to see if the script is ever run."
-);
-
-console.log("Goodbye World!");
-
-process.exit(0);
